c60630d81ba9866d96d01c35ca3cae1022125a52
Satoki <satokiito@gmail.com>
2025-02-11 03:19:54 +0900

    feat: add styling to form

--- src/app/page.js
+++ src/app/page.js
@@ -164,7 +164,7 @@ export default function Home() {
         <textarea
           value={description}
           onChange={(e) => setDescription(e.target.value)}
-          placeholder="Optional description"
+          placeholder="Optional description..."
           className="description-input"
         />
         <button onClick={addTask} className="add-button">
