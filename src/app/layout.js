4c6735f69e201380b1169c29a1b9dc89cba1b313
Satoki <satokiito@gmail.com>
2025-02-10 18:26:37 +0900

    feat: import google font for styling app

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Indie_Flower } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,6 +11,12 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const indieFlower = Indie_Flower({
+  variable: "--font-indie-flower",
+  subsets: ["latin"],
+  weight: "400",
+})
+
 export const metadata = {
   title: "To-Do List",
   description: "A to-do list to keep track of plans.",
@@ -19,7 +25,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} ${indieFlower.variable}`}>
         {children}
       </body>
     </html>
