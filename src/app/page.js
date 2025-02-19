a8ed9945692ee3cd91132f12bf09fa05f84ed1d1
Satoki <satokiito@gmail.com>
2025-02-19 09:29:44 +0900

    feat: import title styling into app

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,7 @@ import CompletedTasks from "@/components/completed-tasks";
 import LogoutButton from "@/components/logout-button";
 
 import "./styles.css";
+import "./styles/title.css"
 import "./styles/tasks.css";
 import "./keyframes/keyframes.css";
 import "./media-queries/media-queries.css";
