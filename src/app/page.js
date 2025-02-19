808f80bbc102628913fb41de80a6402cc5c7c9cf
Satoki <satokiito@gmail.com>
2025-02-19 09:24:16 +0900

    feat: import tasks styling into app

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,7 @@ import CompletedTasks from "@/components/completed-tasks";
 import LogoutButton from "@/components/logout-button";
 
 import "./styles.css";
+import "./styles/tasks.css"
 import "./keyframes/keyframes.css"
 
 export default function Home() {
